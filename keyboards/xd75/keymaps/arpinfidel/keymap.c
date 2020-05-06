2f0e16c489fa2802f4b4f9a8110ff384a00d54ad
ArpinFidel <richardputra2910@gmail.com>
2020-05-07 01:29:32 +0700

    fix: toggle mod backlight

--- keyboards/xd75/keymaps/arpinfidel/keymap.c
+++ keyboards/xd75/keymaps/arpinfidel/keymap.c
@@ -19,10 +19,11 @@
 #define _QW 0
 #define _FN 1
 
-bool BL_MOD_ON = true;
+bool BL_MOD_ON = false;
 
 enum my_kc {
 	A_BL_TG = SAFE_RANGE,
+    A_BL_MD,
     A_OS_LS = OSM(MOD_LSFT),
     A_OS_RS = OSM(MOD_RSFT)
 };
@@ -65,11 +66,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_FN] = { /* FUNCTION */
-	{ KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   RGB_HUD, _______, RGB_HUI, KC_F7,   KC_F8,   KC_F9,   KC_F10  , KC_F11 , KC_F12   },
+	{ KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   RGB_HUD, RESET  , RGB_HUI, KC_F7,   KC_F8,   KC_F9,   KC_F10  , KC_F11 , KC_F12   },
 	{ KC_WH_U, KC_WH_L, KC_WH_R, KC_MS_U, KC_BTN1, KC_BTN2, RGB_SAD, _______, RGB_SAI, KC_LBRC, KC_RBRC, KC_UP  , _______ , KC_EQL , KC_BSLS  },
 	{ KC_WH_D, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN3, RGB_VAD, _______, RGB_VAI, KC_MINS, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______  },
-	{ _______, _______, _______, _______, _______, _______, RGB_RMOD,A_BL_TG, RGB_MOD, _______, _______, _______, _______ , _______, _______  },
-	{ _______, TT(_FN), RGB_TOG, _______, _______, _______, RESET  , _______, RESET  , KC_BTN1, KC_BTN2, _______, _______ , TT(_FN), _______  },}
+	{ _______, _______, _______, _______, _______, _______, RGB_RMOD,_______, RGB_MOD, _______, _______, _______, _______ , _______, _______  },
+	{ _______, TT(_FN), RGB_TOG, _______, _______, _______, A_BL_TG, TT(_FN), A_BL_MD, KC_BTN1, KC_BTN2, _______, _______ , TT(_FN), _______  },}
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -78,17 +79,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			if (record->event.pressed) {
 				// toggle keycaps leds (f5 pin)
 				PORTF ^= (1 << 5);
+			}
+			return false;
+        case A_BL_MD:
+            if (record->event.pressed) {
                 BL_MOD_ON = !BL_MOD_ON;
 			}
 			return false;
-
 		default:
 			return true;
 	}
 }
 
 void led_set_user(uint8_t usb_led) {
-	if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
+	if (BL_MOD_ON && usb_led & (1<<USB_LED_CAPS_LOCK)) {
 		capslock_led_on();
 	} else {
 		capslock_led_off();
@@ -97,16 +101,17 @@ void led_set_user(uint8_t usb_led) {
 
 // Runs whenever there is a layer state change.
 uint32_t layer_state_set_user(uint32_t state) {
-    if (!BL_MOD_ON) return state;
 	uint8_t layer = biton32(state);
 
 	gp100_led_off();
 	gp103_led_off();
 
-	// turns on gp100 (top left led) for odd layers
-	if (layer & (1<<0)) gp100_led_on();
-	// turns on gp103 (top mid led) for layers 2, 6, ...
-	if (layer & (1<<1)) gp103_led_on();
+    if (BL_MOD_ON) {
+        // turns on gp100 (top left led) for odd layers
+        if (layer & (1<<0)) gp100_led_on();
+        // turns on gp103 (top mid led) for layers 2, 6, ...
+        if (layer & (1<<1)) gp103_led_on();
+    }
 
 	return state;
 }
